997b85aab9d20bd046f7de5e1d7eec3749d5e4a3
ElioPeraza <perazaelio@gmail.com>
2024-09-16 21:31:44 -0300

    first update

--- src/Semaforo.jsx
+++ src/Semaforo.jsx
@@ -1,11 +1,28 @@
 import './Semaforo.css';
+import useState from 'react';
+
+
+
 
 export const Semaforo = () => {
+
+  const [light, setlight] = useState("");
+
+  const activelight = (color) => {
+    setLight(light === color ? "" : color );
+
+  };
+
+
+
+
+
+
   return (
     <div id="semaforo">
-      <div className="luz roja"></div>
-      <div className="luz amarilla"></div>
-      <div className="luz verde"></div>
+      <div className={light === 'roja' ? 'luz roja activa':'luz roja'} onClick={() => activelight('roja')}> </div>
+      <div className={"luz amarilla"}></div>
+      <div className={"luz verde"}></div>
     </div>
   )
 };
