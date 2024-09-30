f1e0ac0e8bd4da89986c2eccc7caffc288d8ee08
ElioPeraza <perazaelio@gmail.com>
2024-09-30 10:06:17 -0300

    Finished Project

--- src/Semaforo.jsx
+++ src/Semaforo.jsx
@@ -1,28 +1,46 @@
 import './Semaforo.css';
-import useState from 'react';
+import { useState, useEffect } from 'react';
 
 
 
 
 export const Semaforo = () => {
 
-  const [light, setlight] = useState("");
+  const [light, setLight] = useState("roja");
+  const [active, setActive] = useState(false)
 
-  const activelight = (color) => {
-    setLight(light === color ? "" : color );
+  const activeLight = (light) => {
+    setLight(light)
+  }
 
+
+  useEffect(() => {
+    let autoLight = null;
+    if (active) {
+      autoLight = setInterval(() => {
+        setLight((changeLight) =>
+          changeLight === "roja" ? "verde" :
+            changeLight === "verde" ? "amarilla" : "roja"
+        );
+      }, 3000);
+      
+      return () => clearInterval(autoLight);
+    }
+  }, [active]);
+
+  const handleButtonClick = () => {
+    setActive((activar) => !activar);
   };
 
-
-
-
-
-
   return (
-    <div id="semaforo">
-      <div className={light === 'roja' ? 'luz roja activa':'luz roja'} onClick={() => activelight('roja')}> </div>
-      <div className={"luz amarilla"}></div>
-      <div className={"luz verde"}></div>
+    <div>
+
+      <div id="semaforo">
+        <div className={`luz roja ${light == "roja" ? "activa" : ""}`} onClick={() => activeLight("roja")} ></div>
+        <div className={`luz amarilla ${light == "amarilla" ? "activa" : ""}`} onClick={() => activeLight("amarilla")}></div>
+        <div className={`luz verde ${light == "verde" ? "activa" : ""}`} onClick={() => activeLight("verde")}></div>
+      </div>
+      <button onClick = {handleButtonClick}> {!active ? " Stop" : " Start"} Change light</button>
     </div>
   )
 };
